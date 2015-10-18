947eee3adabf3defb25395adc4371fdb68550d9a
Jonathan Bouzekri <jonathan.bouzekri@gmail.com>
2015-10-18 15:06:41 +0200

    allow additional command to be run in entrypoint

--- 2.7/docker-entrypoint.sh
+++ 2.7/docker-entrypoint.sh
@@ -13,6 +13,12 @@ python setup.py develop
 # Replace env variable in config file (docker linked container)
 envsubst < /opt/dev/conf/config_original.cfg > /opt/dev/conf/config.cfg
 
+# Check if additional packages.sh file exists
+if [[ -f "/opt/dev/conf/packages" ]]
+then
+    . /opt/dev/conf/packages
+fi
+
 # Launch the project
 exec "$@"
 
